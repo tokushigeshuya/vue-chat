fe16e9d9fc5ce826c727a1cba498c0006660984d
tokushigeshuya <narihare1130@gmail.com>
2024-04-20 20:06:23 +0900

    [add]メッセージ閲覧追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter,createWebHistory } from "vue-router";
-import ChatRooms from "@/components/ChatRooms.vue";
+import ChatRooms from "../components/ChatRooms.vue";
+import ChatRoom from '../components/ChatRoom.vue';
 
 const routes = [
-    {path: '/', component: ChatRooms },
+    { path: '/', component: ChatRooms },
+    { path: '/rooms/:id', component: ChatRoom, props: route =>({ roomId: route.params.id }) },
 ];
 
 const router = createRouter({
